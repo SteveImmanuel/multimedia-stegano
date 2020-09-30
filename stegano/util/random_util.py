91b3ed35073600bfa60bf9f7ac77a91f6d6ed486
a-johanes <antoniusjohanes99@gmail.com>
2020-10-01 01:09:09 +0700

    Generalize get_random_sequence to take n dim

--- stegano/util/random_util.py
+++ stegano/util/random_util.py
@@ -1,9 +1,18 @@
 import random
+from typing import Tuple
+
+import numpy as np
+from nptyping import ndarray
 
 
 class RandomUtil:
     @staticmethod
-    def get_random_sequence(min: int, max: int, n: int, seed: int) -> list:
+    def get_random_sequence(min_pos: ndarray, shape: ndarray, n: int, seed: int) -> list:
         random.seed(seed)
-        choices = list(range(min, max + 1))
-        return random.sample(choices, n)
+        min_index = np.ravel_multi_index(min_pos, shape)
+        max_index = np.prod(shape)
+        sequence = random.sample(range(min_index, max_index), n)
+        sequence = list(
+            map(lambda val: (np.unravel_index(val[1], shape), val[0]), enumerate(sequence)))
+        sequence.sort()
+        return sequence
