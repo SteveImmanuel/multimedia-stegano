0025a6e4200d30a4668e2b43e330a1930a6a4f35
jrandiny <jrandiny@gmail.com>
2020-09-30 22:48:58 +0700

    Add method for engine file options

---
+++ stegano/engine/__init__.py
@@ -0,0 +1,2 @@
+from .base_engine import BaseEngine
+from .dummy_engine import DummyEngine
--- stegano/engine/base_engine.py
+++ stegano/engine/base_engine.py
@@ -1,14 +1,41 @@
 from abc import ABC, abstractmethod
 
+from typing import List, Dict
+
 
 class BaseEngine(ABC):
     def conceal(self, file_in_path: str, secret_file_path: str, file_out_path: str):
-        #TODO: validation
+        # TODO: validation
         self._conceal(file_in_path, secret_file_path, file_out_path)
 
     def extract(self, file_in_path: str, extract_file_path: str):
-        #TODO: validation
+        # TODO: validation
         self._extract(file_in_path, extract_file_path)
+
+    @property
+    def conceal_option(self) -> List[Dict[str, str]]:
+        # Return list of dict, each dict value will be used ad the option label
+        return self._get_conceal_option()
+
+    @abstractmethod
+    def _get_conceal_option(self) -> List[Dict[str, str]]:
+        pass
+
+    @property
+    def supported_extensions(self) -> List[str]:
+        return self._get_supported_extensions()
+
+    @abstractmethod
+    def _get_supported_extensions(self) -> List[str]:
+        pass
+
+    @abstractmethod
+    def check_file_supported(self, filepath: str) -> bool:
+        pass
+
+    @abstractmethod
+    def get_max_message(self, filepath: str) -> int:
+        pass
 
     @abstractmethod
     def _conceal(self, file_in_path: str, secret_file_path: str, file_out_path: str):
