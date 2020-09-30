92e6261dcf525ed66cd643b1d17103d05714e8c0
jrandiny <jrandiny@gmail.com>
2020-09-30 15:30:36 +0700

    Add encryption config

---
+++ stegano/gui/widget/encrypt_config/__init__.py
@@ -0,0 +1,47 @@
+from PyQt5.QtWidgets import QGroupBox, QButtonGroup, QRadioButton, QLineEdit, QVBoxLayout, \
+    QHBoxLayout, QLabel
+
+
+class EncryptConfig(QGroupBox):
+    def __init__(self):
+        super(EncryptConfig, self).__init__()
+
+        self._setup_ui()
+
+    def _setup_ui(self):
+        self.setTitle('Encryption Options')
+        self._button_group = QButtonGroup()
+        self._button_group_layout = QHBoxLayout()
+
+        self._encrypt_option = QRadioButton()
+        self._encrypt_option.setText('Enabled')
+        self._encrypt_option.setChecked(True)
+        self._encrypt_option.clicked.connect(self._on_radio_selected)
+        self._no_encrypt_option = QRadioButton()
+        self._no_encrypt_option.setText('Disabled')
+        self._no_encrypt_option.clicked.connect(self._on_radio_selected)
+
+        self._button_group.addButton(self._encrypt_option)
+        self._button_group.setId(self._encrypt_option, 1)
+        self._button_group_layout.addWidget(self._encrypt_option)
+
+        self._button_group.addButton(self._no_encrypt_option)
+        self._button_group.setId(self._no_encrypt_option, 0)
+        self._button_group_layout.addWidget(self._no_encrypt_option)
+
+        self._password_entry_label = QLabel()
+        self._password_entry_label.setText('Password : ')
+        self._password_entry = QLineEdit()
+
+        self._password_entry_layout = QHBoxLayout()
+        self._password_entry_layout.addWidget(self._password_entry_label)
+        self._password_entry_layout.addWidget(self._password_entry)
+
+        self._main_layout = QVBoxLayout()
+        self._main_layout.addLayout(self._button_group_layout)
+        self._main_layout.addLayout(self._password_entry_layout)
+
+        self.setLayout(self._main_layout)
+
+    def _on_radio_selected(self):
+        self._password_entry.setDisabled(self._button_group.checkedId() == 0)
--- stegano/gui/widget/encrypt_tab/__init__.py
+++ stegano/gui/widget/encrypt_tab/__init__.py
@@ -1,5 +1,6 @@
 from PyQt5.QtWidgets import QWidget, QVBoxLayout, QHBoxLayout
 
+from stegano.gui.widget.encrypt_config import EncryptConfig
 from stegano.gui.widget.io_box import IoBox, IoBoxType
 
 
@@ -10,10 +11,9 @@ class EncryptTab(QWidget):
         self._setup_ui()
 
     def _setup_ui(self):
-        self._layout = QVBoxLayout()
-
         self._file_input_box = IoBox(IoBoxType.INPUT, 'Input file')
         self._message_input_box = IoBox(IoBoxType.INPUT, 'Message file')
+        self._encrypt_config_box = EncryptConfig()
         self._file_output_box = IoBox(IoBoxType.OUTPUT, 'Output file')
 
         self._input_layout = QHBoxLayout()
@@ -22,6 +22,7 @@ class EncryptTab(QWidget):
 
         self._main_layout = QVBoxLayout()
         self._main_layout.addLayout(self._input_layout)
+        self._main_layout.addWidget(self._encrypt_config_box)
         self._main_layout.addWidget(self._file_output_box)
 
         self.setLayout(self._main_layout)
