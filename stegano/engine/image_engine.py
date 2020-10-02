4a9ff9fad63b4c006e3fd9a44f96630fe0925c18
jrandiny <jrandiny@gmail.com>
2020-10-02 23:13:46 +0700

    Implement sequential img lsb engine

--- stegano/engine/image_engine.py
+++ stegano/engine/image_engine.py
@@ -45,7 +45,7 @@ class ImageEngine(BaseEngine):
             encryption_key: str,
             config: List[str],
     ) -> None:
-        is_random = True
+        is_random = False
         is_lsb = True
 
         file_in_extension = os.path.splitext(file_in_path)[-1].lower()
@@ -61,73 +61,94 @@ class ImageEngine(BaseEngine):
 
         metadata_len = FileUtil.get_metadata_len(max_file_size) + 1
 
+        secret_file_handle = open(secret_file_path, 'rb')
+
         if is_lsb:
             min_pos = np.unravel_index(metadata_len, image_shape)
 
-            assert is_random
-            metadata.append(1)
-            seed = RandomUtil.get_seed_from_string(encryption_key)
-            sequence = RandomUtil.get_random_sequence(min_pos, image_shape, secret_file_len, seed)
-            sequence.sort(key=lambda x: x[1])
-
-            secret_file_handle = open(secret_file_path, 'rb')
-
+            metadata.append(1 if is_random else 0)
             image_flatten_view = image.ravel()
 
             # Insert metadata
             for i in range(metadata_len):
                 image_flatten_view[i] = (image_flatten_view[i] & 254) | metadata[i]
 
-            for position, idx in sequence:
-                bit_position = idx % 8
-                if bit_position == 0:
-                    current_byte = secret_file_handle.read(1)
+            if is_random:
+                seed = RandomUtil.get_seed_from_string(encryption_key)
+                sequence = RandomUtil.get_random_sequence(min_pos, image_shape, secret_file_len,
+                                                          seed)
+                sequence.sort(key=lambda x: x[1])
 
-                # noinspection PyUnboundLocalVariable
-                secret_bit = ord(current_byte) >> (7 - bit_position) & 1
-                image[position] = (image[position] & 254) | secret_bit
+                for position, idx in sequence:
+                    bit_position = idx % 8
+                    if bit_position == 0:
+                        current_byte = secret_file_handle.read(1)
 
-            imwrite(file_out_path, image, file_in_extension)
-            secret_file_handle.close()
+                    # noinspection PyUnboundLocalVariable
+                    secret_bit = ord(current_byte) >> (7 - bit_position) & 1
+                    image[position] = (image[position] & 254) | secret_bit
+            else:
+                for idx in range(secret_file_len):
+                    bit_position = idx % 8
+                    if bit_position == 0:
+                        current_byte = secret_file_handle.read(1)
+
+                    # noinspection PyUnboundLocalVariable
+                    secret_bit = ord(current_byte) >> (7 - bit_position) & 1
+                    img_idx = idx + metadata_len
+                    image_flatten_view[img_idx] = (image_flatten_view[img_idx] & 254) | secret_bit
+
 
         else:
             raise RuntimeError('Not supported yet :(')
 
+        imwrite(file_out_path, image, file_in_extension)
+        secret_file_handle.close()
+
     @staticmethod
     def extract(file_in_path: str, extract_file_path: str, encryption_key: str) -> None:
-        is_random = True
         is_lsb = True
 
         image = imread(file_in_path)
         image_shape = image.shape
 
+        image_flatten_view = image.ravel()
+
         output_handle = open(extract_file_path, 'wb')
         seed = RandomUtil.get_seed_from_string(encryption_key)
-
 
         if is_lsb:
             max_file_size = int(np.prod(image_shape))
             metadata_len = FileUtil.get_metadata_len(max_file_size) + 1
 
             # Baca metadata
-            metadata_frame = list(image.ravel()[:metadata_len] & 1)
+            metadata_frame = list(image_flatten_view[:metadata_len] & 1)
             is_random = metadata_frame.pop() == 1
-
             secret_file_len, secret_file_ext = FileUtil.extract_metadata(metadata_frame)
 
-            assert is_random
-            min_pos = np.unravel_index(metadata_len, image_shape)
-            sequence = RandomUtil.get_random_sequence(min_pos, image_shape, secret_file_len, seed)
-            sequence.sort(key=lambda x: x[1])
+            if is_random:
+                min_pos = np.unravel_index(metadata_len, image_shape)
+                sequence = RandomUtil.get_random_sequence(min_pos, image_shape, secret_file_len,
+                                                          seed)
+                sequence.sort(key=lambda x: x[1])
 
-            temp_byte = []
-            for position, idx in sequence:
-                temp_byte.append(image[position] & 1)
+                temp_byte = []
+                for position, idx in sequence:
+                    temp_byte.append(image[position] & 1)
 
-                if idx % 8 == 7:
-                    byte = bytes([FileUtil.binary_to_dec(temp_byte)])
-                    output_handle.write(byte)
-                    temp_byte.clear()
+                    if idx % 8 == 7:
+                        byte = bytes([FileUtil.binary_to_dec(temp_byte)])
+                        output_handle.write(byte)
+                        temp_byte.clear()
+            else:
+                temp_byte = []
+                for idx in range(secret_file_len):
+                    temp_byte.append(image_flatten_view[idx + metadata_len] & 1)
+
+                    if idx % 8 == 7:
+                        byte = bytes([FileUtil.binary_to_dec(temp_byte)])
+                        output_handle.write(byte)
+                        temp_byte.clear()
 
             output_handle.close()
 
