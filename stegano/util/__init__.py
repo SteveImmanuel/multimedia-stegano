ffcacb558d83c5949f73657d069d38cf689a0754
SteveImmanuel <steveandreasimanuel@gmail.com>
2020-10-04 17:03:45 +0700

    Move audio util to engine directly

--- stegano/util/__init__.py
+++ stegano/util/__init__.py
@@ -1,4 +1,3 @@
-from .audio_util import AudioUtil
 from .file_util import FileUtil
 from .random_util import RandomUtil
 from .string_util import StringUtil
--- stegano/util/audio_util.py
+++
@@ -1,8 +0,0 @@
-import wave
-import subprocess
-
-
-class AudioUtil:
-    @staticmethod
-    def convert_to_wav(file_in_path: str, file_out_path: str) -> None:
-        subprocess.call(['ffmpeg', '-loglevel', 'quiet', '-i', file_in_path, file_out_path])
